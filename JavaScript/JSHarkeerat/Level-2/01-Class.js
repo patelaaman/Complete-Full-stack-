9aac672895272505e2208d7b5a16c779c3ea93e7
patelaaman <amanpatel51251@gmail.com>
2026-03-30 19:49:00 +0530

    add dats

---
+++ JavaScript/JSHarkeerat/Level-2/01-Class.js
@@ -0,0 +1,14 @@
+const Dog ={ 
+    name : "Doggie",
+    legCouint: 4,
+    speaks: "Bhow bhow"
+}
+
+const Cat ={ 
+    name : "Cat",
+    legCouint: 4,
+    speaks: "Meow"
+}
+
+console.log("Animals :  "+dog["name"]+" "+dog["speaks"])
+console.log("Animals :  "+Cat["name"]+" "+dog["speaks"])
